7ea143469ca34713f1c50b6ef26dfcd3673e5ad9
SATYAJIT6253 <119771949+SATYAJIT6253@users.noreply.github.com>
2024-03-02 10:17:28 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          some changes are there
         </p>
         <a
           className="App-link"
