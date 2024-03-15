2ce64bcc5237535e7e0ad0432562f7ca38caf80f
SATYAJIT6253 <119771949+SATYAJIT6253@users.noreply.github.com>
2024-03-15 09:15:24 +0530

    updated navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Navbar from './Navbar/navbar';
+import Home from './Home/Home';
+import About from './About/About';
+import Project from './Project/Project';
+import Skills from './Skills/Skills';
+import Resume from './Resume/Resume';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          some changes are there
-        </p>
-        
-       
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Project" element={<Project/>}/>
+        <Route path="/Skills" element={<Skills/>}/>
+        <Route path="/Resume" element={<Resume/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './navbar.css';
+
+function Navbar() {
+    
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default Navbar;
